22f71665a2899629dc7a89d930b036eba7176d40
Andre Cirne <dumahk21@gmail.com>
2016-04-05 07:53:08 +0100

    add stdlib on the header

--- header.h
+++ header.h
@@ -1,9 +1,10 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "sys/socket.h"
 #include "arpa/inet.h"
 #include "string.h"
 #include "ctype.h"
 
 //Funções a adicionar
-
+#include "serverfunctions.h"
 #include "Database_handler.h"
