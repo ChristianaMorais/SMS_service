1e0d0169253bf8e54d8f23e465a6ff633f6b4ef5
Andre Cirne <dumahk21@gmail.com>
2016-04-07 15:57:25 +0100

    Criação da matriz

--- server.c
+++ server.c
@@ -1,12 +1,5 @@
 #include "header.h"
 
-typedef struct { //definição do novo tipo utilizador
-	char login[30];
-	char password[30];
-} utilizador;
-
-static utilizador Dados[UserNumber()]; //alem de crirar  matriz correspondente tambem cria o ficheiro se não existir 
-
 int main(int argc, char *argv[]){
 	int state;
    if (argc!=1) // ver se foi iniciado com algum argumento
@@ -15,6 +8,7 @@ int main(int argc, char *argv[]){
   		addUser(argv[2]);
   }
   else{
+    utilizador Dados[UserNumber()];
 
 
   }
