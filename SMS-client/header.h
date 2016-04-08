2767e44f6832cc590cc015103c3176d943e0c5b7
Andre Cirne <dumahk21@gmail.com>
2016-04-08 01:50:42 +0100

    adicionei o novo header

--- SMS-client/header.h
+++ SMS-client/header.h
@@ -1,3 +1,11 @@
 #pragma once
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"    //strlen
+#include "sys/socket.h"    //socket
+#include "arpa/inet.h" //inet_addr
+
+
+
+//files
+#include "functions.h"
