95de49e9697bf929cd7e1784dca6d8d1b6326c93
Andre Cirne <dumahk21@gmail.com>
2016-04-08 21:37:42 +0100

    ficheiros movimentados doutra pasta

---
+++ SMS-server/server.c
@@ -0,0 +1,20 @@
+#include "header.h"
+
+int main(int argc, char *argv[]){
+	int state;
+   if (argc!=1) // ver se foi iniciado com algum argumento
+  {
+  	if(strcmp(argv[1],"adduser")==0)
+  		addUser(argv[2]);
+  }
+  else{
+    DBreader();
+    startServer();
+
+
+  }
+
+  return 0;
+}
+
+
