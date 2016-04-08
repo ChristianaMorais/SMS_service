aa96078c0035949321e4edaa1315835ee352e835
Andre Cirne <dumahk21@gmail.com>
2016-04-08 21:53:34 +0100

    criação do erro se for inciado sem argumentos

---
+++ SMS-client/main.c
@@ -0,0 +1,14 @@
+#include "header.h"
+
+int main(int argc, char const *argv[])
+{
+	if (argc!=0)
+	{
+		/* code */
+	}
+	else{
+		perror("Chamada do cliente sem argumentos!")
+		exit(1);
+	}
+	return 0;
+}
